2a67a897d1f6b6bf70a077d7a6af6d8ca6bc1d7e
Merlin Webster <mjftwebster@gmail.com>
2020-12-29 16:14:02 +0000

    Solved day1 part2

--- lib/advent_of_code_day1.ex
+++ lib/advent_of_code_day1.ex
@@ -2,11 +2,22 @@ defmodule AdventOfCode.Day1 do
   @input_file Path.join(:code.priv_dir(:advent_of_code), "day1_input")
   @intended_sum 2020
 
-  def run() do
+  def run_part1() do
     @input_file
     |> read_input()
     |> find_pairs_with_sum(@intended_sum)
     |> Enum.map(fn {x, y} -> x * y end)
+    |> Enum.uniq()
+    |> List.first()
+  end
+
+  def run_part2() do
+    @input_file
+    |> read_input()
+    |> find_triplets_with_sum(@intended_sum)
+    |> Enum.map(fn {x, y, z} -> x * y * z end)
+    |> Enum.uniq()
+    |> List.first()
   end
 
   @spec read_input(String.t()) :: [integer]
@@ -20,5 +31,14 @@ defmodule AdventOfCode.Day1 do
   @spec find_pairs_with_sum([number], number) :: [{number, number}]
   defp find_pairs_with_sum(input, sum) do
     for x <- input, y <- input, x != y and x + y == sum, do: {x, y}
+  end
+
+  @spec find_triplets_with_sum([number], number) :: [{number, number, number}]
+  defp find_triplets_with_sum(input, sum) do
+    for x <- input,
+        y <- input,
+        z <- input,
+        x != y and y != z and z != x and x + y + z == sum,
+        do: {x, y, z}
   end
 end
